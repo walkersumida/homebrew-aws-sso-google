4fb5ba91b5635696976e21ad4e13d1c3fd7fdae2
WalkerSumida <walkersumida@gmail.com>
2024-11-14 22:14:05 +0900

    chore: update version of aws-sso-google

--- Formula/aws-sso-google.rb
+++ Formula/aws-sso-google.rb
@@ -1,7 +1,7 @@
 class AwsSsoGoogle < Formula
   desc "Acquire AWS STS credentials via Google Workspace SAML in a browser"
   homepage "https://github.com/walkersumida/aws-sso-google"
-  version "0.7.0"
+  version "0.7.1"
 
   head do
     url "https://github.com/walkersumida/aws-sso-google.git"
@@ -9,24 +9,24 @@ class AwsSsoGoogle < Formula
   end
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.0/aws-sso-google_0.7.0_darwin_arm64.tar.gz"
-      sha256 "3f19eba4e7dcef4d69be08646f707551b121cbab2cbba1e97c0ebc0eabf0d01b"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.0/aws-sso-google_0.7.0_darwin_amd64.tar.gz"
-      sha256 "3a00714b54fdcd298e819a83b82d8e96fd73a590e6e4c6c1ac699793243547b7"
+      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.1/aws-sso-google_0.7.1_darwin_amd64.tar.gz"
+      sha256 "427db5e89f5ea865bf654b25aba88f05465b7aec9fa60ca18e231ca35c05f03a"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.1/aws-sso-google_0.7.1_darwin_arm64.tar.gz"
+      sha256 "da7898fab382897c9936f478707fd58152df812d5ae924eaaae7a4536ba4f678"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.0/aws-sso-google_0.7.0_linux_arm64.tar.gz"
-      sha256 "332e491572273103d9be194191b08b6805bdbf3cb6eda1691620cf48a92a00ed"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.0/aws-sso-google_0.7.0_linux_amd64.tar.gz"
-      sha256 "c5c9ff8b8fba9c8d10777d6b54dce422064681c6f2c440bac77bf2449130398a"
+      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.1/aws-sso-google_0.7.1_linux_amd64.tar.gz"
+      sha256 "8dd0b35ad517c09ecbe40735d9fde8ea6b870eb7300746901d1c62c48b816586"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/walkersumida/aws-sso-google/releases/download/v0.7.1/aws-sso-google_0.7.1_linux_arm64.tar.gz"
+      sha256 "5b1aec3da55d27f40523b85b21c54cfffac822ad467f9653ab3dadcd2ed77d98"
     end
   end
 
